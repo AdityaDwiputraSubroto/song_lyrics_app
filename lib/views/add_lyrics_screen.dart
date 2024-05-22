9a14cb58ee9906f48ada4263537137959ea40b9a
AdityaDwiputraSubroto <adityadwiputras23@gmail.com>
2024-05-22 18:28:45 +0700

    add validator

--- lib/views/add_lyrics_screen.dart
+++ lib/views/add_lyrics_screen.dart
@@ -12,11 +12,11 @@ class AddSongTestScreen extends StatefulWidget {
 }
 
 class _AddSongTestScreenState extends State<AddSongTestScreen> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final SongController songController = SongController();
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _artistController = TextEditingController();
-  final TextEditingController _lyricsController =
-      TextEditingController(text: 'add \n add');
+  final TextEditingController _lyricsController = TextEditingController();
   String? _newImagePath;
   String? _imagePath;
 
@@ -70,147 +70,157 @@ class _AddSongTestScreenState extends State<AddSongTestScreen> {
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: SizedBox(
-                    width: 150,
-                    height: 150,
-                    child: InkWell(
-                      onTap: _pickImage,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          border: Border.all(color: Color(0xFFb2855d)),
-                          borderRadius: BorderRadius.circular(
-                              6.0), // Menambahkan border radius
-                        ),
-                        child: _imagePath != null
-                            ? Image.file(File(_imagePath!))
-                            : Icon(Icons.image),
-                      ),
-                    ),
-                  ),
-                ),
-
-                SizedBox(height: 16.0),
-                TextFormField(
-                  controller: _titleController,
-                  decoration: InputDecoration(
-                    labelText: 'Title Song',
-                    labelStyle: TextStyle(color: Color(0xFF0b0302)),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFb2855d)),
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFb2855d)),
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                  ),
-                  style: TextStyle(color: Color(0xFF0b0302)),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the title';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16.0),
-                TextFormField(
-                  controller: _artistController,
-                  decoration: InputDecoration(
-                    labelText: 'Artist',
-                    labelStyle: TextStyle(color: Color(0xFF0b0302)),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFb2855d)),
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFb2855d)),
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                  ),
-                  style: TextStyle(color: Color(0xFF0b0302)),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the artist';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 16.0),
-                TextFormField(
-                  controller: _lyricsController,
-                  decoration: InputDecoration(
-                    labelText: 'Lyrics',
-                    labelStyle: TextStyle(color: Color(0xFF0b0302)),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFb2855d)),
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color(0xFFb2855d)),
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                  ),
-                  maxLines: 3,
-                  keyboardType: TextInputType.multiline,
-                  style: TextStyle(color: Color(0xFF0b0302)),
-                ),
-                SizedBox(height: 16.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                        String? lyrics = await songController.fetchLyrics(
-                            context,
-                            _artistController.text.trim(),
-                            _titleController.text.trim());
-                        if (lyrics != null) {
-                          setState(() {
-                            _lyricsController.text = lyrics;
-                          });
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFFb2855d),
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: InkWell(
+                        onTap: _pickImage,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                            border: Border.all(color: Color(0xFFb2855d)),
+                            borderRadius: BorderRadius.circular(
+                                6.0), // Menambahkan border radius
+                          ),
+                          child: _imagePath != null
+                              ? Image.file(File(_imagePath!))
+                              : Icon(Icons.image),
                         ),
                       ),
-                      child: Text('Fetch Lyrics'),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        final song = Song(
-                          title: _titleController.text,
-                          artist: _artistController.text,
-                          lyrics: _lyricsController.text,
-                          imageName:
-                              _newImagePath != null ? _newImagePath : null,
-                          favorited: false, // Default to false
-                        );
-                        print(_lyricsController.text);
-                        songController.addSong(context, song,
-                            _imagePath != null ? _imagePath : '');
-                        //Navigator.pop(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0xFFb2855d),
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6.0),
-                        ),
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                      labelText: 'Title Song',
+                      labelStyle: TextStyle(color: Color(0xFF0b0302)),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb2855d)),
+                        borderRadius: BorderRadius.circular(6.0),
                       ),
-                      child: Text('Add Song'),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb2855d)),
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
                     ),
-                  ],
-                ),
-              ],
+                    style: TextStyle(color: Color(0xFF0b0302)),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the title';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _artistController,
+                    decoration: InputDecoration(
+                      labelText: 'Artist',
+                      labelStyle: TextStyle(color: Color(0xFF0b0302)),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb2855d)),
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb2855d)),
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                    ),
+                    style: TextStyle(color: Color(0xFF0b0302)),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the artist';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _lyricsController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the lyrics';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Lyrics',
+                      labelStyle: TextStyle(color: Color(0xFF0b0302)),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb2855d)),
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb2855d)),
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                    ),
+                    maxLines: 3,
+                    keyboardType: TextInputType.multiline,
+                    style: TextStyle(color: Color(0xFF0b0302)),
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          String? lyrics = await songController.fetchLyrics(
+                              context,
+                              _artistController.text.trim(),
+                              _titleController.text.trim());
+                          if (lyrics != null) {
+                            setState(() {
+                              _lyricsController.text = lyrics;
+                            });
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFb2855d),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                        ),
+                        child: Text('Fetch Lyrics'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            final song = Song(
+                              title: _titleController.text,
+                              artist: _artistController.text,
+                              lyrics: _lyricsController.text,
+                              imageName:
+                                  _newImagePath != null ? _newImagePath : null,
+                              favorited: false, // Default to false
+                            );
+                            print(_lyricsController.text);
+                            songController.addSong(context, song,
+                                _imagePath != null ? _imagePath : '');
+                          }
+                          //Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFb2855d),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                        ),
+                        child: Text('Add Song'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
